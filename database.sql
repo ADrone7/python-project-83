9587b42c39d13ff806304c639393b3a9a523c1ad
ADrone <aleksandron1997@gmail.com>
2025-08-13 09:11:50 +0300

    Drop tables if exist

--- database.sql
+++ database.sql
@@ -1,10 +1,13 @@
-CREATE TABLE IF NOT EXISTS urls (
+DROP TABLE IF EXISTS urls;
+DROP TABLE IF EXISTS url_checks;
+
+CREATE TABLE urls (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL UNIQUE,
     created_at DATE
 );
 
-CREATE TABLE IF NOT EXISTS url_checks (
+CREATE TABLE url_checks (
     id SERIAL PRIMARY KEY,
     url_id integer REFERENCES urls (id),
     status_code INT,
